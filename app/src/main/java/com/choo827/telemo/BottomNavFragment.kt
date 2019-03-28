26010e36cfc1b5d6730077ac898f183374ac8b2f
choo827 <choo827@naver.com>
2019-03-28 23:48:57 +0900

    bottom navigation 생성

---
+++ app/src/main/java/com/choo827/telemo/BottomNavFragment.kt
@@ -0,0 +1,48 @@
+package com.choo827.telemo
+
+
+import android.content.Context
+import android.os.Bundle
+import android.view.Gravity
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import kotlinx.android.synthetic.main.fragment_bottom_nav.*
+
+
+/**
+ * A simple [Fragment] subclass.
+ *
+ */
+class BottomNavigationDrawerFragment: BottomSheetDialogFragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_bottom_nav, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        navigation_view.setNavigationItemSelectedListener { menuItem ->
+            // Bottom Navigation Drawer menu item clicks
+            when (menuItem!!.itemId) {
+                R.id.nav1 -> context!!.toast("1번 클릭")
+                R.id.nav2 -> context!!.toast("2번 클릭")
+            }
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+            true
+        }
+    }
+
+    // This is an extension method for easy Toast call
+    fun Context.toast(message: String) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.BOTTOM, 0, 600)
+        toast.show()
+    }
+
+}
--- app/src/main/java/com/choo827/telemo/MainActivity.kt
+++ app/src/main/java/com/choo827/telemo/MainActivity.kt
@@ -1,12 +1,46 @@
 package com.choo827.telemo
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setSupportActionBar(bottomAppBar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.bottomappbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item!!.itemId) {
+//            R.id.app_bar_fav -> toast("Fav menu item is clicked!")
+            R.id.app_bar_search -> toast("Search menu item is clicked!")
+            android.R.id.home -> {
+                val bottomNavDrawerFragment = BottomNavigationDrawerFragment()
+                bottomNavDrawerFragment.show(supportFragmentManager, bottomNavDrawerFragment.tag)
+            }
+//            R.id.app_bar_settings -> toast("Settings item is clicked!")
+        }
+
+        return true
+    }
+
+    fun Context.toast(message: CharSequence) {
+        val toast = Toast.makeText(this, message, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.BOTTOM, 0, 325)
+        toast.show()
     }
 }
