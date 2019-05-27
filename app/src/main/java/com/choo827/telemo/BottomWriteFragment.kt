931793c7f536226712ac25f6d8a84949d308d804
choo827 <choo827@naver.com>
2019-05-27 23:53:03 +0900

    전번 추가 레이아웃 full-screen dialog로 변경

--- app/src/main/java/com/choo827/telemo/BottomWriteFragment.kt
+++ app/src/main/java/com/choo827/telemo/BottomWriteFragment.kt
@@ -2,18 +2,22 @@ package com.choo827.telemo
 
 
 import android.os.Bundle
-import android.telephony.PhoneNumberFormattingTextWatcher
-import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
+import androidx.fragment.app.DialogFragment
 import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.fragment_bottom_write.*
 import kotlinx.android.synthetic.main.fragment_bottom_write.view.*
 import java.util.*
 
 
-class BottomWriteFragment : RoundedBottomSheetDialogFragment() {
+class BottomWriteFragment : DialogFragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setStyle(STYLE_NORMAL, R.style.FullScreenDialogStyle)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,37 +25,47 @@ class BottomWriteFragment : RoundedBottomSheetDialogFragment() {
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_bottom_write, container, false)
-        dialog?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
-        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_NOTHING)
+        view.toolbar.setNavigationIcon(R.drawable.ic_close_drawer)
+        view.toolbar.setNavigationOnClickListener { dismiss() }
 
         val db = FirebaseFirestore.getInstance()
         val userId = arguments?.getString("userUid")
 
-        view.phoneNumber.addTextChangedListener(PhoneNumberFormattingTextWatcher())
-
-        view.saveBtn.setOnClickListener {
-            val phoneNumber = view.phoneNumber.text.toString()
-            val name = view.name.text.toString()
-            val etc = view.etc.text.toString()
-
-            val data = PhoneNumber(phoneNumber, name, etc, timeGenerator())
-            db.collection(userId.toString()).document(timeGenerator()).set(data)
-//                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
-//                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
-
-            view.phoneNumber.text = Editable.Factory.getInstance().newEditable("")
-            view.name.text = Editable.Factory.getInstance().newEditable("")
-
-            dismiss()
-        }
-
-        view.expandBtn.setOnClickListener {
-            dismiss()
-        }
+//        view.phoneNumber.addTextChangedListener(PhoneNumberFormattingTextWatcher())
+//
+//        view.saveBtn.setOnClickListener {
+//            val phoneNumber = view.phoneNumber.text.toString()
+//            val name = view.name.text.toString()
+//            val etc = view.etc.text.toString()
+//
+//            val data = PhoneNumber(phoneNumber, name, etc, timeGenerator())
+//            db.collection(userId.toString()).document(timeGenerator()).set(data)
+////                .addOnSuccessListener { Log.d(TAG, "DocumentSnapshot successfully written!") }
+////                .addOnFailureListener { e -> Log.w(TAG, "Error writing document", e) }
+//
+//            view.phoneNumber.text = Editable.Factory.getInstance().newEditable("")
+//            view.name.text = Editable.Factory.getInstance().newEditable("")
+//
+//            dismiss()
+//        }
+//
+//        view.expandBtn.setOnClickListener {
+//            dismiss()
+//        }
 
 
 
         return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val dialog = dialog
+        if (dialog != null) {
+            val width = ViewGroup.LayoutParams.MATCH_PARENT
+            val height = ViewGroup.LayoutParams.MATCH_PARENT
+            dialog.window.setLayout(width, height)
+        }
     }
 
     private fun timeGenerator(): String {
